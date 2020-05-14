ea4ddaed738c95571aa9867ee9f7defc746e5e5b
Tommy Brumley <tommy.brumley95@gmail.com>
2020-05-13 20:32:57 -0400

    tests and birthdays added

--- weberapi/weber/models.py
+++ weberapi/weber/models.py
@@ -5,7 +5,8 @@ from django.db import models
 class Villager(models.Model):
     name = models.CharField(max_length=20)
     personality = models.TextField()
+    birthday = models.CharField(max_length=40, default='01/01')
 
     def __str__(self):
         """TEST"""
-        return self.title
+        return self.name
--- weberapi/weber/tests.py
+++ weberapi/weber/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from .models import Villager
 
 # Create your tests here.
+
+class VillagerModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        Villager.objects.create(name = 'Weber')
+        Villager.objects.create(personality = 'Lazy')
+        Villager.objects.create(birthday = '06/30')
+
+    def test_name(self):
+        test = Villager.objects.get(id=1)
+        expected_obj = f'{test.name}'
+        self.assertEquals(expected_obj, 'Weber')
+
+    def test_personality(self):
+        test = Villager.objects.get(id=2)
+        expected_obj = f'{test.personality}'
+        self.assertEquals(expected_obj, 'Lazy')
+
+    def test_birthday(self):
+        test = Villager.objects.get(id=3)
+        expected_obj = f'{test.birthday}'
+        self.assertEquals(expected_obj, '06/30')
+
